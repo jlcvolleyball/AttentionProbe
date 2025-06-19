8b1de999f1520e7e24cb18dc8424cd4d877d4aaf
Jessica Chen <jlcvolleyball@gmail.com>
2025-06-19 16:21:02 -0400

    Added experimentation and tokenization_testing, allowing us to input a sentence or word and see its tokenized representation

--- attention_visualizations.py
+++ attention_visualizations.py
@@ -20,8 +20,8 @@ def find_difference(t1, t2):
 
 # input and tokenizing
 # "The man gave the woman his jacket. Who owned the jacket, the man or the woman?"
-input_text1 = "The man showed the dogs his dinner. Who owned the dinner, the man or the dogs?"
-input_text2 = "The man showed the dogs their dinner. Who owned the dinner, the man or the dogs?"
+input_text1 = "The woman hit the criminal with the stick. Who had the stick?"
+input_text2 = "The woman hit the criminal with the scar. Who had the scar?"
 inputs = tokenizer([input_text1, input_text2],
                    padding=True,
                    return_tensors="pt",
